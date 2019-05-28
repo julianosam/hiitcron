002baa0b9489c9ae4e73d8efab8fd1937706e329
Juliano Sanches <juliano.sanches@adp.com>
2019-05-28 15:29:42 -0400

    Animations and multiple sections

--- src/app/chronometer/chronometer.component.ts
+++ src/app/chronometer/chronometer.component.ts
@@ -1,9 +1,29 @@
-import { Component, ElementRef, OnInit, ViewChild, Renderer2 } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { trigger, state, transition, animate, style } from '@angular/animations';
 
 @Component({
     selector: 'hc-chronometer',
     templateUrl: './chronometer.component.html',
-    styleUrls: ['./chronometer.component.scss']
+    styleUrls: ['./chronometer.component.scss'],
+    animations: [
+        trigger('expandCollapse', [
+            // ...
+            state('expand', style({
+                width: '19rem',
+                height: '19rem',
+            })),
+            state('collapse', style({
+                width: '18rem',
+                height: '18rem',
+            })),
+            transition('* => collapse', [
+                animate('100ms ease-out')
+            ]),
+            transition('* => expand', [
+                animate('100ms ease-in')
+            ]),
+        ]),
+    ],
 })
 export class ChronometerComponent implements OnInit {
 
@@ -11,7 +31,40 @@ export class ChronometerComponent implements OnInit {
 
     @ViewChild('pie')
     pie: ElementRef;
+    @ViewChild('highlight')
+    highlight: ElementRef;
+    @ViewChild('sections')
+    sections: ElementRef;
 
+    plan = [{
+        type: 'low',
+        duration: 6
+    }, {
+        type: 'high',
+        duration: 2
+    }, {
+        type: 'low',
+        duration: 6
+    }, {
+        type: 'high',
+        duration: 2
+    },{
+        type: 'low',
+        duration: 6
+    }, {
+        type: 'high',
+        duration: 2
+    }, {
+        type: 'low',
+        duration: 6
+    }, {
+        type: 'high',
+        duration: 2
+    }];
+
+    isHighlighted = 'collapse';
+
+    currentSection = 0;
     elapsedSeconds = 0;
     totalSeconds = 30;
     start = 0;
@@ -20,6 +73,26 @@ export class ChronometerComponent implements OnInit {
 
 
     ngOnInit() {
+
+        let gradientSections = 'conic-gradient(';
+        let start = 0;
+        let end = 0;
+        let accum = 0;
+
+        const totalTime = this.plan.reduce((t, section) => t + section.duration, 0);
+
+        this.plan.forEach(p => {
+
+            start = accum * 100 / totalTime;
+            end = start + (p.duration * 100 / totalTime);
+            accum += p.duration;
+            gradientSections += `${p.type === 'high' ? '#faa' : '#bca'} ${start}% ${end}%,`;
+        });
+
+        gradientSections += `#eee 0)`;
+
+        this.sections.nativeElement.style.setProperty('background-image', gradientSections);
+
         this._restartTimer();
     }
 
@@ -34,11 +107,17 @@ export class ChronometerComponent implements OnInit {
 
     private _restartTimer() {
 
+        // this.totalSeconds = this.plan.reduce((t, i) => i.duration + t, 0);
+        this.totalSeconds = this.plan[this.currentSection].duration;
+        this._updateSection();
+        this.isHighlighted = 'expand';
+
         this.elapsedSeconds = 0;
         this.start = 0;
         this._progressTime(0);
 
-        
+
+
 
         // this._handler = setInterval(() => {
 
@@ -47,24 +126,26 @@ export class ChronometerComponent implements OnInit {
         // }, 1000);
 
     }
-    
+
 
     private _progressTime(t) {
 
         this.start = this.start > 0 ? this.start : t;
-        
+
         const progress = t - this.start;
         this.elapsedSeconds = Math.floor(progress / 1000);
 
-        this._updatePie( progress * 100 / (this.totalSeconds * 1000) );
+        this._updatePie(progress * 100 / (this.totalSeconds * 1000));
 
-        if (this.elapsedSeconds <= this.totalSeconds) {
+        if (this.elapsedSeconds < this.totalSeconds) {
 
             window.requestAnimationFrame((t) => this._progressTime(t));
-            
+
         } else {
             // clearInterval(this._handler);
-            this.elapsedSeconds = this.totalSeconds;
+            this.currentSection++;
+            this.isHighlighted = 'collapse';
+            setTimeout(() => this._restartTimer(), 100);
         }
     }
 
@@ -75,6 +156,24 @@ export class ChronometerComponent implements OnInit {
         // this.renderer.setStyle(this.pie.nativeElement, 'background-image', style);
 
         this.pie.nativeElement.style.setProperty('--completed-segment', completed);
+    }
+
+    private _updateSection() {
+        const currentPlanSection = this.plan[this.currentSection];
+        const totalTime = this.plan.reduce((t, section) => t + section.duration, 0);
+        const totalElapsed = this.plan.reduce((t, section, index) => {
+            return index < this.currentSection ? section.duration + t : t;
+        }, 0);
+
+        const highStart = totalElapsed * 100 / totalTime;
+        const highEnd = highStart + (currentPlanSection.duration * 100 / totalTime);
+        const highColor = currentPlanSection.type === 'low' ? '#bca' : '#faa';
+
+        this.highlight.nativeElement.style.setProperty('--highlight-start', highStart);
+        this.highlight.nativeElement.style.setProperty('--highlight-end', highEnd);
+        this.highlight.nativeElement.style.setProperty('--highlight-color', highColor);
+
+
     }
 
 }
